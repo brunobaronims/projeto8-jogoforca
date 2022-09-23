6b7d0ba5024021b4c137ee7505c9216436c446ce
Bruno Baroni <brunobaronims@gmail.com>
2022-09-23 15:55:06 -0300

    Add directories, styled-components

---
+++ src/App.js
@@ -0,0 +1,24 @@
+//import Jogo from "./components/Jogo";
+//import Letras from "./components/Letras";
+//import Chute from "./components/Chute";
+
+export default function App () {
+  return (
+    <main>
+      <section className='jogo'>
+        <div className='game-left'>
+
+        </div>
+        <div className='game-right'>
+          
+        </div>
+      </section>
+      <section className='letras'>
+
+      </section>
+      <section className='chute'>
+
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Letras.js
@@ -0,0 +1,3 @@
+export default function Letras () {
+  return null;
+}
